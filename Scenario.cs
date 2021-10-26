68b193b7c712c5b5d49033fc72809fc9b39e7f4f
Asanka Nawagamuwage <asanka.nawagamuwage@securepay.com.au>
2021-10-27 00:49:36 +1100

    Scenario handler

---
+++ Scenario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobot
+{
+	public class Scenario
+	{
+		public class Placement
+		{
+			public Point InitialPoint { get; set; }
+			public Direction Direction { get; set; }
+		}
+
+		public enum Actions
+		{
+			MOVE = 0,
+			LEFT = 1,
+			RIGHT = 2,
+			REPORT = 3
+		}
+
+		public List<Actions> RobotActions { get; set; }
+	}
+}
